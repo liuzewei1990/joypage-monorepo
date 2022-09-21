14c72f0d02f5bd68231430178044b0ad9239f1c9
Liu zewei <543347844@qq.com>
2022-09-21 18:07:15 +0800

    1

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
     },
     rules: {
         "vue/multi-word-component-names": 0,
+        "vue/valid-v-for": 0,
         "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
         "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
         "no-alert": 0, //禁止使用alert confirm prompt
